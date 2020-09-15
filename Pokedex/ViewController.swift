3f53318f3cf19657b4c0f427ffcd702cfa405df6
AndresFelipe27 <anndresfee@hotmail.com>
2020-09-14 21:48:23 -0500

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Felipe18 on 14/09/20.
+//  Copyright © 2020 Felipe18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
